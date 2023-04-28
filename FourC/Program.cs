b28356091d2fc1b5ef90b44b7467aea921b2db67
SergeiBelkin <128678208+SergeiBelkin@users.noreply.github.com>
2023-04-28 08:53:06 +0300

    задача 41

---
+++ FourC/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+// int PozitivSumm = 0;
+// string EndedText = " ";
+// while (EndedText == " ")
+// {
+    
+//     Console.WriteLine("введите число или введите end для завершения");
+//     string EnteredText = Console.ReadLine();
+    
+//     if (EnteredText == "end")
+//     {
+//         EndedText = EnteredText;
+//         break;
+//     }
+//     int a = int.Parse(EnteredText);
+//     if (a > 0)
+//     {
+//         PozitivSumm++;
+//     }
+    
+
+// }
+// Console.WriteLine(PozitivSumm);
+
+
+
